12ddf7a61909a8e4215c5e88d9bd929a30185ebe
League Student <league@iMac-29.attlocal.net>
2020-03-13 16:56:02 -0700

    not fun

---
+++ src/_01_prime_or_not/prime.java
@@ -0,0 +1,14 @@
+package _01_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class prime {
+	public static void main(String[] args) {
+		while (true) {
+			String input = JOptionPane.showInputDialog(null, "Insert any number");
+			int number = parseint(input);
+			
+		
+		}
+	}
+}
