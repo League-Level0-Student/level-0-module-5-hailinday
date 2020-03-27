40491d7b5de1acc1bdb75f101e880a5a8baa1c44
root <root@50e4535a2674>
2020-03-27 23:59:09 +0000

    Prime or not prime

--- src/_01_prime_or_not/prime.java
+++ src/_01_prime_or_not/prime.java
@@ -6,9 +6,15 @@ public class prime {
 	public static void main(String[] args) {
 		while (true) {
 			String input = JOptionPane.showInputDialog(null, "Insert any number");
-			int number = parseint(input);
-			
-		
+			int number = Integer.parseInt(input);
+			for (int i = 1; i <= number; i++) {
+			if (number % i == number && number % i == 1) {
+				JOptionPane.showMessageDialog(null, "The number is prime");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "The number is not prime");
+				}
+			}
 		}
 	}
 }
